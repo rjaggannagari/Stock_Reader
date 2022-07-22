c8e80b7d17581a400907b59d2a53c5b92d982458
rjaggannagari <rjaggannagari@vt.edu>
2022-07-22 10:34:15 -0400

    added a graph function

--- stockreader.py
+++ stockreader.py
@@ -2,17 +2,38 @@
 import yfinance
 import datetime
 import pandas
+import plotly.graph_objs
 
 # Each stock on yahoo finance has an acronym
-stock_input = input("Input a Stock Acronym: ")
+stock_input = input("Input a Stock: ")
 
 # Sees if you want to view specific data through a time period
-view_dates = input("Do you want to see the given stock through a given period (Yes or No): ")
+view_dates = input("Do you want to see the above stock in a given time period (Yes or No)? ")
+
+# Sees if you want to view the graph of the given time period
+view_graphs = input("Do you want to see the recent data of the inputted stock in a graph format (Yes or No)? ")
 
 # Checks to see whether the input is yes or no
 timeperiod_data = False
 if view_dates == "yes" or view_dates == "Yes":
     timeperiod_data = True
+
+# Checks to see whether the input is yes or no
+graph_data = False
+if view_graphs == "yes" or view_graphs == "Yes":
+    if timeperiod_data is False:
+        graph_data = True
+        period_data = input("Enter the days in between: ")
+        # Only valid intervals are 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo
+        interval_data = input("Enter the interval: ")
+        data = yfinance.download(tickers=stock_input, period=period_data, interval=interval_data, rounding=True)
+        figure = plotly.graph_objs.Figure()
+        figure.add_trace(plotly.graph_objs.Candlestick())
+        figure.add_trace(
+            plotly.graph_objs.Candlestick(x=data.index, open=data['Open'], high=data['High'], low=data['Low'],
+                                          close=data['Close'], name='market data'))
+        figure.update_layout(title='Given Stock Share Price', yaxis_title='Stock Price (USD)')
+        figure.show()
 
 # Takes in an input for the start date and the end date
 if timeperiod_data:
